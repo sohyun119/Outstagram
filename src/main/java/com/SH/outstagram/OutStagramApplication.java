25ebfeee0d75c334bd82ea2bd1304cfa4b6d3677
Sohyun <sh-119@naver.com>
2022-01-25 21:31:34 +0900

    [이소현] Outstagram - 환경설정(bulid, databasdConfig, application.yml)

---
+++ src/main/java/com/SH/outstagram/OutStagramApplication.java
@@ -0,0 +1,13 @@
+package com.SH.outstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutStagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutStagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SH/outstagram/OutStagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SH.outstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutStagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
