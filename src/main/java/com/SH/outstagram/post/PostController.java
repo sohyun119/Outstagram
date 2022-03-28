d179c4ac4960ea8370dad04610e22336c69cf14e
Sohyun <sh-119@naver.com>
2022-03-28 19:17:34 +0900

    // 배포 후 수정 테스트

--- src/main/java/com/SH/outstagram/post/PostController.java
+++ src/main/java/com/SH/outstagram/post/PostController.java
@@ -29,7 +29,7 @@ public class PostController {
 			HttpServletRequest request
 			, Model model
 			) {
-		
+		// 
 		HttpSession session = request.getSession();
 		int thisId = (Integer)session.getAttribute("userId");
 		
